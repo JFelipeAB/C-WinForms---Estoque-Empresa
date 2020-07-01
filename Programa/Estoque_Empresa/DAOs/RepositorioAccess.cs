c1605b0d612cd39dc35fea25987f5ff6bd10a556
unknown <joao-alves-borges@hotmail.com>
2020-07-01 14:09:55 -0300

    1.3 80% concluido

--- Programa/Estoque_Empresa/DAOs/RepositorioAccess.cs
+++ Programa/Estoque_Empresa/DAOs/RepositorioAccess.cs
@@ -14,20 +14,38 @@ namespace DAOs
 
         static public void Insert(Estoque item)
         {
-            OleDbConnection conn = new OleDbConnection();
-            conn.ConnectionString = Conexao.pathPadrao;
 
-            string sql = "INSERT into Estoque(Nome, Disponivel, Manutencao, Local, Data, observacao) Values (";
-            sql += item.Nome + ","+ item.Disponivel+ "," + item.Manutencao + "," + item.Local + "," + item.Data + "," + item.Observacao+")";
+            //OleDbConnection conn = new OleDbConnection(Conexao.pathPadrao);     PARAMITERSS
+            //conn.Open();
 
+
+            //string sql = "INSERT INTO Estoque (nome, Disponivel, Manutencao, Local, Data, Observacao) VALUES(@nome, @Disponivel, @Manutencao, @Local, @Data, @Observacao)";
+
+            //OleDbCommand cmd = new OleDbCommand(sql, conn);
+
+            //cmd.Parameters.AddWithValue("@nome", item.Nome);
+            //cmd.Parameters.AddWithValue("@Disponivel", item.Disponivel);
+            //cmd.Parameters.AddWithValue("@Manutencao", item.Manutencao);
+            //cmd.Parameters.AddWithValue("@Local", item.Local);
+            //cmd.Parameters.AddWithValue("@Data", oleDbType.Date).Value = item.Data.ToString());
+            //cmd.Parameters.AddWithValue("@Observacao", item.Observacao);
+            //cmd.ExecuteNonQuery();
+            //conn.Close();
+
+            OleDbConnection conn = new OleDbConnection(Conexao.pathPadrao);
+            
+
+            string sql = "INSERT into Estoque ( nome, Disponivel, Manutencao, Local, Data, Observacao) Values ";
+            //sql += $"('{item.Nome}' , {item.Disponivel} , {item.Manutencao}, '{item.Local}' , #{item.Data}# , '{item.Observacao}')";
+            sql += $"('teste' , 1 , 1, 'localteste' , #'26/07/2020'# , 'Observacao')";
             OleDbCommand cmd = new OleDbCommand(sql);
             cmd.Connection = conn;
 
             conn.Open();
-            cmd.ExecuteNonQuery();           
+            cmd.ExecuteNonQuery();
             conn.Close();
 
-            //RepositorioAccess.Escreve(sql);
+
         }
         static public void Insert(Registro item)
         {
@@ -69,7 +87,7 @@ namespace DAOs
             //define a string de conexao com provedor caminho e nome do banco de dados
             string strProvider = Conexao.pathPadrao; //"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=c:\\dados\\Cadastro.mdb";
             //define a instrução SQL
-            string strSql = "SELECT "+nome+" FROM " + tabela;
+            string strSql = $"SELECT {nome} FROM {tabela}";
 
             //cria a conexão com o banco de dados
             OleDbConnection con = new OleDbConnection(strProvider);
